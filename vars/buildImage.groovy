bb6b7e27546da8c2bf9eacaafb6f16ba7fdde844
Leandro Beretta <lea.beretta@gmail.com>
2019-02-21 21:17:56 -0300

    fixes

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -6,6 +6,7 @@ class BuildImageParameters {
     String project = ""
     String application
     String baseImage
+    String image
     String artifactsDir = "./"
 }
 
@@ -17,8 +18,9 @@ def call(BuildImageParameters buildImageParameters) {
     openshift.withCluster(buildImageParameters.clusterUrl, buildImageParameters.clusterToken) {
         openshift.withProject(buildImageParameters.project) {
             if (!openshift.selector("bc", buildImageParameters.application).exists()) {                
-                createBuilder(project: buildImageParameters.project, application: buildImageParameters.application, baseImage. buildImageParameters.baseImage)
+                createBuilder(project: buildImageParameters.project, application: buildImageParameters.application, baseImage: buildImageParameters.baseImage, image: buildImageParameters.image)
             }
+            
             openshift.selector("bc", buildImageParameters.application).startBuild("--from-dir=${buildImageParameters.artifactsDir}", "--wait=true")
         }
     }
--- vars/createBuilder.groovy
+++ vars/createBuilder.groovy
@@ -1,21 +1,22 @@
 #!/usr/bin/env groovy
 
-class CreateBuilderParameteres {
+class CreateBuilderParameters {
     String clusterUrl = ""
     String clusterToken = ""
     String project = ""
     String application
+    String image
     String baseImage
 }
 
-def call(createBuilderParameteres) {
-    call(new CreateBuilderParameteres(createBuilderParameteres))
+def call(createBuilderParameters) {
+    call(new CreateBuilderParameters(createBuilderParameters))
 }
 
-def call(CreateBuilderParameteres createBuilderParameteres) {
-    openshift.withCluster(createBuilderParameteres.clusterUrl, createBuilderParameteres.clusterToken) {
-        openshift.withProject(createBuilderParameteres.project) {
-            openshift.newBuild("--image-stream=${createBuilderParameteres.baseImage}", "--name=${createBuilderParameteres.application}", "--binary=true");                   
+def call(CreateBuilderParameters createBuilderParameters) {
+    openshift.withCluster(createBuilderParameters.clusterUrl, createBuilderParameters.clusterToken) {
+        openshift.withProject(createBuilderParameters.project) {
+            openshift.newBuild("--image-stream=${createBuilderParameters.baseImage}", "--name=${createBuilderParameters.application}", "--binary=true", "--to=${createBuilderParameters.image}");                   
         }
     }
 }
