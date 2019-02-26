843807e683a3e7424495eaf8962e6557842ec851
Leandro Beretta <lea.beretta@gmail.com>
2019-02-26 01:37:39 -0300

    script included

---
+++ resources/scripts/setup.sh
@@ -0,0 +1,18 @@
+#! /usr/bin/env bash
+
+oc create -f templates/java-pipeline.yaml -n openshift
+oc create -f templates/node-pipeline.yaml -n openshift
+
+oc new-project hello-thorntail-dev
+oc new-project hello-thorntail-test
+oc new-project hello-thorntail-prod
+
+oc new-app --template=jenkins-ephemeral --name=jenkins -n hello-thorntail-dev
+
+oc adm policy add-role-to-user edit system:serviceaccount:hello-thorntail-dev:jenkins -n hello-thorntail-test
+oc adm policy add-role-to-user edit system:serviceaccount:hello-thorntail-dev:jenkins -n hello-thorntail-prod
+
+oc new-app --template java-pipeline -p PARAM_GIT_REPO=https://github.com/leandroberetta/hello-thorntail.git -p PARAM_GIT_BRANCH=master -p PARAM_APP_NAME=hello-thorntail -n hello-thorntail-dev
+
+
+
